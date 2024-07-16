99dfea73651eceb344412388bb3ada2c0f4b1b88
tushar <tusharkumarshah14394@gmail.com>
2024-07-16 17:37:31 +0530

    tree with recursion

---
+++ 543. Diameter of Binary Tree.cpp
@@ -0,0 +1,30 @@
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int height(TreeNode* node, int& diameter){
+        if(node==nullptr)
+            return 0;
+        int lh=height(node->left,diameter);
+        int rh=height(node->right,diameter);
+        diameter=max(diameter,lh+rh);
+        return 1+max(lh,rh);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        height(root,diameter);
+        return diameter;
+    }
+};
