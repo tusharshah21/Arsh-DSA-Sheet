18be9ff32109fe1cdc8d6a3c0e28b38903d6ea95
tushar <tusharkumarshah14394@gmail.com>
2024-07-18 14:32:00 +0530

    optimal solution apart from hash function

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -1,15 +1,35 @@
 
 
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         unordered_set<int> set;
+//         int len = nums.size();
+//         for (int i = 0; i < len; ++i) {
+//             if (!set.insert(nums[i]).second) {
+//                 return nums[i];
+//             }
+//         }
+//         return len;
+//     }
+// };
+
+// optimal solution 
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set<int> set;
-        int len = nums.size();
-        for (int i = 0; i < len; ++i) {
-            if (!set.insert(nums[i]).second) {
-                return nums[i];
-            }
+        int fast=0;
+        int slow=0;
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return len;
+        return slow;
     }
 };
